26232bcdd3deba6be9a9107ce989c610db9fd180
staszad <stas_ady@protonmail.com>
2022-01-30 10:03:08 +0100

    minor changes in cpp file and removing some unnecessary files

--- heap/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-while(tmp->parent()){
-    //     if(tmp->value > tmp->parent()->value){
-    //         swap(tmp, tmp->parent());
-    //         tmp = tmp->parent();
-    //     } else 
-    //         break;
-    // }
